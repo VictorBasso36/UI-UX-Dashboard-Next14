000660c5c0c4f95cdc421cb835569ed8ccf6279a
root <root@DESKTOP-SKBK36U>
2023-12-12 22:06:24 -0300

    test dependencies

--- app/Dashboard/Home/chart2.tsx
+++ app/Dashboard/Home/chart2.tsx
@@ -1,52 +1,52 @@
+"use client"
 import React, { useContext } from 'react';
 import { DashboardContext } from '../layoutProvider';
 import styles from './Home.module.css';
 import dayjs from 'dayjs';
+import ReactApexChart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 import { Line } from '@ant-design/charts';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   Tooltip,
-//   PointElement,
-//   LineElement,
-//   BarElement,
-// } from "chart.js";
-// import { Chart } from 'react-chartjs-2';
+import dynamic from 'next/dynamic';
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Tooltip,
-//   BarElement
-// );
 
 export default function Chart2() {
   const { start, end } = DashboardContext();
-  const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
-  ];
-
-  const config = {
-    data,
-    height: 400,
-    xField: 'year',
-    yField: 'value',
-    padding: [50],
-    point: {
-      size: 5,
-      shape: 'diamond',
-    },
+  const options: ApexOptions = {
+    series: [14, 23, 21, 17, 15, 10, 12, 17, 21],
+   
+              chart: {
+                type: 'polarArea',
+              },
+              stroke: {
+                colors: ['#fff']
+              },
+              fill: {
+                opacity: 0.8
+              },
+              responsive: [{
+                breakpoint: 480,
+                options: {
+                  chart: {
+                    width: 200
+                  },
+                  legend: {
+                    position: 'bottom'
+                  }
+                }
+              }]
+        
+          
   };
-  return <Line {...config} />;
+  // const series = [{
+  //   data: [31, 40, 28, 51, 42, 109, 100]
+  // }]
+  
+    return (
+      <ReactApexChart
+      options={options}
+      series={options.series}
+      type="polarArea"
+      height={350}
+    />
+    )
 }
